7360430b066e27970169ad9ac38424a81c796aaf
Daniel FM <dfigueroa@instacrops.com>
2019-06-23 04:23:00 -0400

    Updates

---
+++ precache-manifest.326b8ddfd96cef35c1e32be74f5444f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0833ae25e05fab09c07dfd8406559466",
+    "url": "/movies-web/index.html"
+  },
+  {
+    "revision": "94f1f4bcf3e591b9e662",
+    "url": "/movies-web/static/css/2.ad9eb4b5.chunk.css"
+  },
+  {
+    "revision": "6023e064b784e8e90787",
+    "url": "/movies-web/static/css/main.817f96f6.chunk.css"
+  },
+  {
+    "revision": "94f1f4bcf3e591b9e662",
+    "url": "/movies-web/static/js/2.6a9168ca.chunk.js"
+  },
+  {
+    "revision": "6023e064b784e8e90787",
+    "url": "/movies-web/static/js/main.c8bcc195.chunk.js"
+  },
+  {
+    "revision": "80de6c5f7f530a59702e",
+    "url": "/movies-web/static/js/runtime~main.174440c0.js"
+  }
+]);
